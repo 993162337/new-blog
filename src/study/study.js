fd208e2e32bd8188a1411fc1e9c573753c4456dc
993162337 <993162337@qq.com>
2015-11-12 01:05:23 +0800

    查看文章内容功能

--- src/study/study.js
+++ src/study/study.js
@@ -19,6 +19,13 @@ let Study = React.createClass({
     return {
       new: null,
       list: [],
+      index: 0,
+      artical: {
+        title: "",
+        content: "",
+        time: "",
+        mark: "",
+      }
     }
   },
 
@@ -26,7 +33,8 @@ let Study = React.createClass({
     $.getJSON(this.props.url)
       .done(d => { 
         this.setState({
-          list: d
+          list: d,
+          index: 0,
         })
        })
       .fail(d => { console.log(d) })
@@ -43,31 +51,84 @@ let Study = React.createClass({
 
         this.setState({
           list: newList
+        }, function() {
+          setTimeout('$(".study-content").ellipsis(350)', 5)
         })
        })
       .fail((d, error) => { console.log(error.message) })
   },
 
+  getContent(e) {
+    let id = $(e.target).closest("div").attr("id")
+    $.getJSON(this.props.url)
+      .done(d => {
+        d.map(item => {
+          if(item.id == id) {
+            console.log(item)
+            this.setState({
+              artical: item,
+              index: 1,
+            })
+          }
+        })
+      })
+  },
+
+  pullBack() {
+    this.updateData()
+    setTimeout('$(".study-content").ellipsis(350)', 5)
+  },
+
   render() {
-    let articalList = []
-    this.state.list.map((item, key) => {
-      return articalList.push(
-        <div className="study-item" key={key}>
-          <a>{ item.title }</a><br />
-          <span className="study-tag">{ item.mark }</span>
-          <span className="study-time">{ item.time }</span>
-          <p className="study-content">{ item.content }</p>
-          <span className="study-more">more>></span>
-        </div>
-      )
-    })
+    let content = [
+      <ArticalList list={ this.state.list } getContentCB={ this.getContent } />,
+      <ArticalContent data={ this.state.artical } backCB={ this.pullBack }/>
+    ]
+
     return (
       <div className="study-list">
         <Tab onChangeCB={ this.tabChanged }/>
-        { articalList }
+        { content[this.state.index] }
       </div>
     )
   }
 });
+
+let ArticalList = React.createClass({
+  render() {
+    let articalList = []
+
+    this.props.list.map((item, key) => {
+      return articalList.push(
+        <div className="study-item" id={ item.id } key={key} >
+          <a onClick={ this.props.getContentCB }>{ item.title }</a><br />
+          <span className="study-tag">{ item.mark }</span>
+          <span className="study-time">{ item.time }</span>
+          <p className="study-content">{ item.content }</p>
+          <span className="study-more" onClick={ this.props.getContentCB }>more&gt;&gt;</span>
+        </div>
+      )
+    })
+
+    return (
+      <div className="artical-list">
+        { articalList }
+      </div>
+    )
+  }
+})
+
+let ArticalContent = React.createClass({
+  render() {
+    return (
+      <div className="artical">
+        <p className="button"><a className="btn btn-default" type="button" onClick={ this.props.backCB } >&lt;&lt;</a></p>
+        <h2>{ this.props.data.title }</h2>
+        <p className="artical-date">{ this.props.data.time }</p>
+        <p className="artical-content">{ this.props.data.content }</p>
+      </div>
+    )
+  }
+})
 
 export default Study
