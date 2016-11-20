b2cabd87558a3021358309a3714493e210a24b98
wuzeng <wuzeng.li@fugetech.com>
2016-11-20 22:01:38 +0800

    Bug Fix

--- static/global/init.js
+++ static/global/init.js
@@ -3,7 +3,7 @@
 * @Date:   2016-07-09 23:07:00
 * @Email:  woolson.lee@gmail.com
 * @Last modified by:   woolson
-* @Last modified time: 2016-11-20 02:11:55
+* @Last modified time: 2016-11-20 19:11:47
 */
 
 import "./array"
@@ -12,7 +12,9 @@ import "./string"
 window.Global = {}
 
 if((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
-    Global.equiv = "mobile"
+  Global.equiv = "mobile"
+  if(location.host !== "m.woolson.cn") location.replace("http://m.woolson.cn")
 }else {
-    Global.equiv = "pc"
+  Global.equiv = "pc"
+  if(location.host == "m.woolson.cn") location.replace("http://www.woolson.cn")
 }
